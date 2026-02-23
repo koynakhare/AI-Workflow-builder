e88ef8a1a295354101b36b2020f471a5b1449f04
koynakhare <koynakhare29@gmail.com>
2026-02-23 20:36:01 +0530

    Updated base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { getStorageItem, handleApiError } from "../utils/helper";
+import { config } from "../config";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "/",
+  baseURL: config.baseURL || "/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
